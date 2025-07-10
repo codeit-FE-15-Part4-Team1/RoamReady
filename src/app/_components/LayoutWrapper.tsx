c472e8290d98857e4d50f0897b5f6ffd00431f76
김서연 <rainbow2870@naver.com>
2025-07-11 01:25:37 +0900

    (#56) feat: footer 하단 고정을 위한 min-h-screen 스타일  추가

--- src/app/_components/LayoutWrapper.tsx
+++ src/app/_components/LayoutWrapper.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 
 import Header from '@/shared/components/layouts/header/Header';
+import Footer from '@/shared/components/layouts/footer/Footer';
 
 type Props = {
   children: ReactNode;
@@ -11,9 +12,10 @@ export default function LayoutWrapper({ children, withHeader }: Props) {
   return (
     <div className='min-h-[100dvh] min-w-[100dvw]'>
       {withHeader && <Header />}
-      <div className='tablet:px-32 desktop:px-40 desktop:text-40 text-14 mx-auto max-w-1200 px-24'>
+      <div className='tablet:px-32 desktop:px-40 desktop:text-40 text-14 mx-auto min-h-screen max-w-1200 px-24'>
         {children}
       </div>
+      {withHeader && <Footer />}
     </div>
   );
 }
