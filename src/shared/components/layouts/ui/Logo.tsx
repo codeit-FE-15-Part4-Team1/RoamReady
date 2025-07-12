b48caefc38b55d7a04e4413c2f1d59e985e0c069
김서연 <rainbow2870@naver.com>
2025-07-12 22:56:34 +0900

    (#73) refactor: header pr 리뷰 반영

---
+++ src/shared/components/layouts/ui/Logo.tsx
@@ -0,0 +1,41 @@
+import { ROUTES } from '@/shared/constants/route';
+import { cn } from '@/shared/libs/cn';
+import Image from 'next/image';
+import Link from 'next/link';
+
+/**
+ * Logo 컴포넌트 입니다.
+ *
+ * 메인 페이지로 이동하는 링크 역할을 하는 로고 컴포넌트입니다.
+ * 기본 로고 경로(`/logos/logo-3-black.svg`)를 사용하거나, `src`를 통해 다른 이미지로 대체할 수 있습니다.
+ * 또한 `className`을 통해 추가적인 스타일 커스터마이징이 가능합니다.
+ *
+ * @param {string} [props.className] - 추가적인 Tailwind CSS 클래스명
+ * @param {string} [props.src] - 로고 이미지의 경로 (기본값: `/logos/logo-3-black.svg`)
+ *
+ * @example
+ * ```tsx
+ * <Logo />
+ * <Logo src="/logos/logo-2-black.svg"/>
+ * ```
+ */
+export default function Logo({
+  className,
+  src,
+}: {
+  className?: string;
+  src?: string;
+}) {
+  return (
+    <Link href={ROUTES.Main}>
+      <div
+        className={cn(
+          'tablet:w-130 tablet:h-100 relative h-70 w-100 shrink-0',
+          className,
+        )}
+      >
+        <Image src={src ? src : '/logos/logo-3-black.svg'} fill alt='Logo' />
+      </div>
+    </Link>
+  );
+}
