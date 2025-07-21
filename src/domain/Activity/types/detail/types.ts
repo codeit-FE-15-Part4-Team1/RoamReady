1a75e0e6220b60b61ee8e84896c93e0b756c6f15
김서연 <rainbow2870@naver.com>
2025-07-22 00:08:10 +0900

    (#173) feat: mock 데이터 생성

--- src/domain/Activity/types/detail/types.ts
+++ src/domain/Activity/types/detail/types.ts
@@ -22,3 +22,34 @@ export interface Review {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface ReviewList {
+  reviews: Review[];
+  totalCount: number;
+  averageRating: number;
+}
+
+export interface Activity {
+  id: number;
+  userId: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  address: string;
+  bannerImageUrl: string;
+  rating: number;
+  reviewCount: number;
+  createdAt: string;
+  updatedAt: string;
+  subImages: {
+    id: number;
+    imageUrl: string;
+  }[];
+  schedules: {
+    id: number;
+    date: string;
+    startTime: string;
+    endTime: string;
+  }[];
+}
