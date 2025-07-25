ae4bccce965bd0aa81ef3cc5f58ea35f02c34978
Jaehyun Park <hirouth@naver.com>
2025-07-25 16:49:06 +0900

    (#112) feat: 푸터 레이아웃 및 내용 일부 수정

--- src/shared/components/layouts/footer/Footer.tsx
+++ src/shared/components/layouts/footer/Footer.tsx
@@ -19,25 +19,31 @@ import Logo2 from '@/shared/assets/logos/LogoTextTwoline';
  */
 export default function Footer() {
   return (
-    <footer className='border-t border-gray-50 py-10'>
+    <footer className='border-t border-gray-50 py-30'>
       <Container>
         {/* 푸터 전체 컨테이너: 좌우 영역 분할 */}
         <div className='flex justify-between'>
           {/* 좌측 영역: 로고, 브랜드 슬로건, CopyRight sign */}
-          <div className='flex flex-col gap-10'>
-            <Logo2 className='h-70 w-70' />
-            <div className='flex flex-col gap-3'>
+          <div className='font-size-12 flex flex-col gap-20'>
+            <div className='space-y-10'>
+              <Logo2 className='w-70' />
               <span className='text-gray-800'>
-                Start your journey with RoamReady
+                Start your Activity with RoamReady
               </span>
-              <span className='text-gray-300'>
-                © 2025 RoamReady, Codeit Sprint Part4 Team1
-              </span>
+            </div>
+            <div className='flex flex-col'>
+              <p className='text-gray-300'>
+                © 2025 Codeit, Inc.
+                <br />
+                Designed and developed by Team RoamReady (FE15 Part4 Team1)
+                <br />
+                based on Codeit curriculum.
+              </p>
             </div>
           </div>
 
           {/* 우측 영역: GitHub 링크 및 팀원 정보 */}
-          <div className='flex flex-col items-end gap-10'>
+          <div className='font-size-12 flex flex-col items-end gap-10'>
             <a
               aria-label='GitHub repository'
               href='https://github.com/codeit-FE-15-Part4-Team1/RoamReady'
@@ -51,7 +57,9 @@ export default function Footer() {
                 height={30}
               />
             </a>
-            <p>김서연 | 박재현 | 송시은 | 유용민</p>
+            <div className='flex flex-col items-end text-gray-800'>
+              <p>김서연 | 박재현 | 송시은 | 유용민</p>
+            </div>
           </div>
         </div>
       </Container>
