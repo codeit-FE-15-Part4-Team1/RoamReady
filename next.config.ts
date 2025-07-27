cb52fdc9e7289496dc58f48eb70ebc6c787d18b7
김서연 <rainbow2870@naver.com>
2025-07-27 17:33:42 +0900

    (#237) feat: next Image confing 설정

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,9 @@ if (
 const nextConfig: NextConfig = {
   pageExtensions,
   reactStrictMode: true,
+  images: {
+    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
