5a99543d563d296a57cb897d1b8dae0a4b7dd279
김서연 <rainbow2870@naver.com>
2025-07-28 00:38:51 +0900

    (#237) feat: header dropdown 및 avatar 연결

--- src/shared/components/layouts/header/AuthMenu.tsx
+++ src/shared/components/layouts/header/AuthMenu.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
+import Avatar from '@/shared/components/ui/avatar';
+import Dropdown from '@/shared/components/ui/dropdown';
+import { ROUTES } from '@/shared/constants/routes';
+import { useSignout } from '@/shared/hooks/useSignout';
 import type { User } from '@/shared/slices/userSlice';
 
 interface AuthMenuProps {
@@ -15,7 +20,9 @@ interface AuthMenuProps {
  *
  */
 export default function AuthMenu({ user }: AuthMenuProps) {
-  // const signout = useSignout();  이렇게 가져올 수 있어요.
+  const signout = useSignout();
+  const router = useRouter();
+
   return (
     <div className='flex items-center justify-center gap-20'>
       <Image src='/icons/bell.svg' alt='알림 아이콘' width={24} height={24} />
@@ -25,12 +32,20 @@ export default function AuthMenu({ user }: AuthMenuProps) {
         <div className='h-20 w-1 self-center bg-gray-100' />
 
         {/* 프로필 아바타*/}
-        <div className='h-30 w-30 rounded-full bg-black' />
-        {/* user를 사용해야 PR 프리뷰가 생성되서 우선 아래코드 추가했습니다.  */}
-        {user.profileImageUrl}
+        <Dropdown.Root>
+          <Dropdown.Trigger>
+            <Avatar profileImageUrl={user.profileImageUrl ?? ''} />
+          </Dropdown.Trigger>
+          <Dropdown.Menu menuClassName='top-40'>
+            <Dropdown.Item onClick={signout}>로그아웃</Dropdown.Item>
+            <Dropdown.Item onClick={() => router.push(ROUTES.MYPAGE.INFO)}>
+              마이페이지
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown.Root>
 
         {/* 유저 이름 */}
-        <span className='font-size-14'>닉네임</span>
+        <span className='font-size-14'>{user.nickname}</span>
       </div>
     </div>
   );
