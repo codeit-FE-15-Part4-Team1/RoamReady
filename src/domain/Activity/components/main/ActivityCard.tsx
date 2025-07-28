51d6a207744bccfc52db94d7cd4490a880c9d793
Jaehyun Park <hirouth@naver.com>
2025-07-28 22:41:13 +0900

    (#112) feat: 액티비티 카드 link 컴포넌트 래핑

--- src/domain/Activity/components/main/ActivityCard.tsx
+++ src/domain/Activity/components/main/ActivityCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 import CategoryBadge from '@/domain/Activity/components/main/CategoryBadge';
@@ -19,7 +20,7 @@ export default function ActivityCard({
   activity,
   className,
 }: ActivityCardProp) {
-  const { bannerImageUrl, title, price, rating, reviewCount } = activity;
+  const { id, bannerImageUrl, title, price, rating, reviewCount } = activity;
 
   const [hasImageError, setHasImageError] = useState<boolean | undefined>(
     false,
@@ -30,49 +31,51 @@ export default function ActivityCard({
   }, [bannerImageUrl]);
 
   return (
-    <article
-      className={cn(
-        'flex w-full max-w-330 transform flex-col rounded-3xl bg-white p-6 shadow-lg transition-transform duration-300 hover:-translate-y-2',
-        className,
-      )}
-    >
-      <figure className='relative aspect-[5/6] overflow-hidden rounded-3xl'>
-        {hasImageError ? (
-          <div className='bg-brand-1 flex-center absolute inset-0'>
-            <LogoSymbol className='text-brand-2 w-[40%]' />
-          </div>
-        ) : (
-          <Image
-            src={bannerImageUrl}
-            alt={`${title} 액티비티 이미지`}
-            fill
-            className='object-cover'
-            onError={() => setHasImageError(true)}
-            sizes='(max-width: 768px) 50dvw, (max-width: 1024px) 25dvw, 20dvw'
-          />
+    <Link href={`/activities/${id}`}>
+      <article
+        className={cn(
+          'flex w-full max-w-330 transform flex-col rounded-3xl bg-white p-6 shadow-lg transition-transform duration-300 hover:-translate-y-2',
+          className,
         )}
+      >
+        <figure className='relative aspect-[5/6] overflow-hidden rounded-3xl'>
+          {hasImageError ? (
+            <div className='bg-brand-1 flex-center absolute inset-0'>
+              <LogoSymbol className='text-brand-2 w-[40%]' />
+            </div>
+          ) : (
+            <Image
+              src={bannerImageUrl}
+              alt={`${title} 액티비티 이미지`}
+              fill
+              className='object-cover'
+              onError={() => setHasImageError(true)}
+              sizes='(max-width: 768px) 50dvw, (max-width: 1024px) 25dvw, 20dvw'
+            />
+          )}
 
-        <div className='absolute inset-0 flex items-start justify-between p-12'>
-          <CategoryBadge activity={activity} />
-          <LikeButton activity={activity} />
-        </div>
-      </figure>
+          <div className='absolute inset-0 flex items-start justify-between p-12'>
+            <CategoryBadge activity={activity} />
+            <LikeButton activity={activity} />
+          </div>
+        </figure>
 
-      <section className='w-auto p-8 text-gray-900'>
-        <div className='font-size-14 line-clamp-1 truncate font-semibold'>
-          {title}
-        </div>
-        <div className='font-size-12 flex items-center truncate text-gray-800'>
-          <span className='font-semibold'>
-            ₩ {price.toLocaleString()}
-            /인 ㆍ
-          </span>
-          <span className='truncate'>
-            <span className='text-yellow-500'>★</span> {rating.toFixed(1)}{' '}
-            <span className='text-gray-400'>({reviewCount})</span>
-          </span>
-        </div>
-      </section>
-    </article>
+        <section className='w-auto p-8 text-gray-900'>
+          <div className='font-size-14 line-clamp-1 truncate font-semibold'>
+            {title}
+          </div>
+          <div className='font-size-12 flex items-center truncate text-gray-800'>
+            <span className='font-semibold'>
+              ₩ {price.toLocaleString()}
+              /인 ㆍ
+            </span>
+            <span className='truncate'>
+              <span className='text-yellow-500'>★</span> {rating.toFixed(1)}{' '}
+              <span className='text-gray-400'>({reviewCount})</span>
+            </span>
+          </div>
+        </section>
+      </article>
+    </Link>
   );
 }
