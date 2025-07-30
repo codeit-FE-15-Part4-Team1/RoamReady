9071b98eab66eec086d3304954409bb97eae69b7
김서연 <rainbow2870@naver.com>
2025-07-30 20:07:05 +0900

    (#240) refactor: next.config image 도메인 수정

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,14 @@ const nextConfig: NextConfig = {
   pageExtensions,
   reactStrictMode: true,
   images: {
-    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
