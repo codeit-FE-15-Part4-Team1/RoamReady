7e12308195a3ac23d539358c877d9bc68e259713
김서연 <rainbow2870@naver.com>
2025-08-02 12:11:49 +0900

    (#414) refactor: 예약 성공 시 선택값 초기화

--- src/domain/Activity/components/detail/Reservation/ReservationPC.tsx
+++ src/domain/Activity/components/detail/Reservation/ReservationPC.tsx
@@ -61,6 +61,7 @@ export default function ReservationPC({
       return;
     }
 
+    setSelectedDate(null);
     showSuccess(result.message);
   };
 
--- src/domain/Activity/components/detail/Reservation/ReservationTablet.tsx
+++ src/domain/Activity/components/detail/Reservation/ReservationTablet.tsx
@@ -62,6 +62,7 @@ export default function ReservationTablet({
       return;
     }
 
+    setSelectedDate(null);
     showSuccess(result.message);
   };
 
