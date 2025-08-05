85014fc8ca63338c557aac2270b1078e672f7974
Yongmin0423 <yym21345@gmail.com>
2025-08-05 11:31:10 +0900

    (#518) fix: 미들웨어 matcher변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -179,5 +179,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|api/auth/kakao/signin).*)',
+  ],
 };
