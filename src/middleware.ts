ee94188bb12fd804094d6321e2fcbbdac628ca65
Sienna <sgoldenbird@gmail.com>
2025-08-06 19:58:58 +0900

    (#473) fix: oauth 404문제 try2

--- src/middleware.ts
+++ src/middleware.ts
@@ -169,8 +169,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    // '/((?!_next/static|_next/image|favicon.ico|api/auth/kakao/signin).*)',
-    '/((?!_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
